12ca6ead9f9c8387617bac98e1559c8a66cff662
FE Tony <fe.tony@example.com>
2025-12-10 15:01:41 +0700

    xoa het

--- README.md
+++
@@ -1,1 +0,0 @@
-# htmldrone1
--- threeflower/js/config.js
+++
@@ -1,8 +0,0 @@
-// Cấu hình và constants
-
-// Màu sắc cho hoa sen
-const LOTUS_COLOR = 0xFF69B4; // Hồng
-const DRONE_COUNT = 500;
-
-// Biến global
-let globalSpeedMultiplier = 1.0;
